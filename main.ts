e0741611f888d2975d3b28f41a25fbc33b0f430e
ro0k23 <125355361+ro0k23@users.noreply.github.com>
2023-02-28 10:41:27 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(tempmin)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(tempmax)
+})
+let tempmin = 0
+let tempmax = 0
+let température = input.temperature()
+tempmax = température
+tempmin = température
+basic.forever(function () {
+    basic.showString(".")
+    tempmin = tempmin
+    if (température < tempmin) {
+        tempmin = température
+    }
+    if (température > tempmax) {
+        tempmax = température
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+})
